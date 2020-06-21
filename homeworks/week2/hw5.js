cabc7de61a58d43fe1a4158bd8ecc7b462d2621f
sage90180 <sage90180@gmail.com>
2020-06-21 19:15:24 +0800

    week2

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,17 @@
 function join(arr, concatStr) {
-  
+    var newStr = arr[0]
+    for (var i = 1; i < arr.length; i++) {
+        newStr += concatStr + arr[i]
+    }
+    return newStr
 }
 
 function repeat(str, times) {
-  
+    var newStr = ''
+    for (var i = 0; i < times; i++) {
+        newStr += str
+    }
+    return newStr
 }
 
 console.log(join(['a'], '!'));
