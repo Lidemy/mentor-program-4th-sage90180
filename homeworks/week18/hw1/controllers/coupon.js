3d0a5ee772153876e61c36dab37ec611c227e9a9
sage90180 <sage90180@gmail.com>
2020-10-28 23:07:45 +0800

    hw18

---
+++ homeworks/week18/hw1/controllers/coupon.js
@@ -0,0 +1,80 @@
+const db = require('../models');
+
+const { Coupon } = db;
+const couponController = {
+  handleAdd: async (req, res, next) => {
+    try {
+      const {
+        img, name, content, price, expire,
+      } = req.body;
+      if (!img || !name || !content || !price || !expire) {
+        req.flash('errorMessage', '請填好，填滿！！');
+        return next();
+      }
+      await Coupon.create({
+        img,
+        name,
+        content,
+        price,
+        expire,
+      });
+      req.flash('errorMessage', '新增成功！');
+      return res.redirect('/admin');
+    } catch (err) {
+      req.flash('errorMessage', err.toString());
+      return next();
+    }
+  },
+  updateCoupon: async (req, res, next) => {
+    try {
+      const {
+        img, name, content, price, expire,
+      } = req.body;
+      if (!img || !name || !content || !price || !expire) {
+        req.flash('errorMessage', '請填好，填滿！！');
+        return next();
+      }
+      const coupons = await Coupon.findOne({
+        where: {
+          id: req.params.id,
+        },
+      });
+      await coupons.update({
+        img,
+        name,
+        content,
+        price,
+        expire,
+      });
+      req.flash('errorMessage', '修改成功！');
+      return res.redirect('/admin');
+    } catch (err) {
+      req.flash('errorMessage', err.toString());
+      return next();
+    }
+  },
+  deleteCoupon: async (req, res, next) => {
+    const { username } = req.session;
+    if (!username) {
+      req.flash('errorMessage', '請先登入。');
+      return res.render('login');
+    }
+    try {
+      const coupons = await Coupon.findOne({
+        where: {
+          id: req.params.id,
+        },
+      });
+      await coupons.update({
+        delete: '1',
+      });
+      req.flash('errorMessage', '刪除成功！');
+      return res.redirect('/admin');
+    } catch (err) {
+      req.flash('errorMessage', err.toString());
+      return next();
+    }
+  },
+};
+
+module.exports = couponController;
---
+++ homeworks/week18/hw1/controllers/user.js
@@ -0,0 +1,137 @@
+const db = require('../models');
+
+const {
+  User, Type, Dish, Booking, Coupon, Faq,
+} = db;
+const userController = {
+  login: (req, res) => {
+    res.render('login');
+  },
+  logout: (req, res) => {
+    req.session.username = null;
+    res.redirect('/login');
+  },
+  handleLogin: async (req, res, next) => {
+    try {
+      const {
+        username,
+        password,
+      } = req.body;
+      if (!username || !password) {
+        req.flash('errorMessage', '請填好，填滿！！');
+        return next();
+      }
+      const user = await User.findOne({
+        where: {
+          username,
+          password,
+        },
+      });
+      if (!user) {
+        req.flash('errorMessage', '帳號/密碼錯誤喔！');
+        return next();
+      }
+      req.session.username = user.username;
+      return res.redirect('/admin');
+    } catch (err) {
+      req.flash('errorMessage', err.toString());
+      return next();
+    }
+  },
+  index: async (req, res, next) => {
+    try {
+      const types = await Type.findAll({
+        raw: true,
+        where: {
+          delete: null,
+        },
+      });
+      const dishes = await Dish.findAll({
+        raw: true,
+        where: {
+          delete: null,
+        },
+      });
+      const coupons = await Coupon.findAll({
+        raw: true,
+        where: {
+          delete: null,
+        },
+      });
+      const bookings = await Booking.findAll({
+        raw: true,
+        where: {
+          delete: null,
+        },
+      });
+      const faqs = await Faq.findAll({
+        raw: true,
+        where: {
+          delete: null,
+        },
+        order: ['order'],
+      });
+      return res.render('index', {
+        types,
+        dishes,
+        coupons,
+        bookings,
+        faqs,
+      });
+    } catch (err) {
+      req.flash('errorMessage', err.toString());
+      return next();
+    }
+  },
+  admin: async (req, res, next) => {
+    try {
+      const { username } = req.session;
+      if (!username) {
+        req.flash('errorMessage', '請先登入。');
+        return res.render('login');
+      }
+      const types = await Type.findAll({
+        raw: true,
+        where: {
+          delete: null,
+        },
+      });
+      const dishes = await Dish.findAll({
+        raw: true,
+        where: {
+          delete: null,
+        },
+      });
+      const coupons = await Coupon.findAll({
+        raw: true,
+        where: {
+          delete: null,
+        },
+      });
+      const bookings = await Booking.findAll({
+        raw: true,
+        where: {
+          delete: null,
+        },
+      });
+      const faqs = await Faq.findAll({
+        where: {
+          delete: null,
+        },
+        order: ['order'],
+      });
+      return res.render('admin', {
+        types,
+        dishes,
+        coupons,
+        bookings,
+        faqs,
+      });
+    } catch (err) {
+      req.flash('errorMessage', err.toString());
+      return next();
+    }
+  },
+};
+
+module.exports = userController;
